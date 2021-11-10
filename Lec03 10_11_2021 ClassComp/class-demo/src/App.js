371a05925f72ec01e85232dc2a54f2f7291c098f
nc <31476486+nirc7@users.noreply.github.com>
2021-11-10 15:24:53 +0200

    clac

--- Lec03 10_11_2021 ClassComp/class-demo/src/App.js
+++ Lec03 10_11_2021 ClassComp/class-demo/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import FCPerson from './FunctionalComps/FCPerson';
 import CCStudnet from './ClassComps/CCStudent';
+import CCCalc from './ClassComps/CCCalc';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <CCCalc/>
         <CCStudnet grade={99}/>
         <FCPerson id={7} name={"avi"} />
         <FCPerson id={8} name={"benny"} />
